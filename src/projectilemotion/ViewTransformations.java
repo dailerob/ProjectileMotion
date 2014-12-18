b6a9f8919d183042e2726132c9041c71dd9dfd03
dailerob <dailey.roberto@gmail.com>
2014-12-18 17:01:36 -0600

    created view transformations class to preform rotations and perspective adjustment

---
+++ src/projectilemotion/ViewTransformations.java
@@ -0,0 +1,63 @@
+package projectilemotion;
+
+/**
+ * Created by Roberto on 12/18/2014.
+ */
+public class ViewTransformations {
+
+    static int width;
+
+    public ViewTransformations(int width)
+    {
+        this.width = width;
+    }
+
+
+
+    /**
+     *
+     * @param preRotation - the coordinates of the item before the roatation.
+     * @return - the coordinates of the item after the rotation about the xy plane.
+     */
+    static double [] xRotation (double [] preRotation, double xRadian)
+    {
+        double [] rotated = {Math.cos(xRadian)*preRotation[0]+Math.sin(xRadian)
+                *preRotation[2],preRotation[1],Math.cos(xRadian)*preRotation[2]
+                -Math.sin(xRadian)*preRotation[0]};
+        return rotated;
+    }
+
+    /**
+     *
+     * @param preRotation - the coordinates of the item before the roatation.
+     * @return - the coordinates of the item after the rotation about the yz plane.
+     */
+    static double [] yRotation (double [] preRotation, double yRadian)
+    {
+        double [] rotated = {preRotation[0],Math.cos(yRadian)*preRotation[1]
+                +Math.sin(yRadian)*preRotation[2],Math.sin(yRadian)
+                *preRotation[1]-Math.cos(yRadian)*preRotation[2]};
+        return rotated;
+    }
+
+    /**
+     *
+     * @param preRotation - the coordinates of the item before the roatation.
+     * @return - the coordinates of the item after the rotation about the xz plane.
+     */
+    static double [] zRotation (double [] preRotation, double zRadian)
+    {
+        double [] rotated = {Math.cos(zRadian)*preRotation[0]+Math.sin(zRadian)
+                *preRotation[1],Math.cos(zRadian)*preRotation[1]-
+                Math.sin(zRadian)*preRotation[0],preRotation[2]};
+        return rotated;
+    }
+
+    static double perspectiveAdjustment(double zoom, double zDepth){
+        return width/(width+2/Math.atan(1.047+zoom)*zDepth);
+    }
+
+
+
+
+}//class viewTransformations
